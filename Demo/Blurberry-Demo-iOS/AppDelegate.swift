5c6edfa7530ce9c6cd43d9f9eeaea926babfecd0
Pavel <puzyrev.pavel@yahoo.com>
2020-09-09 12:38:13 +0300

    Added basic demo project;

---
+++ Demo/Blurberry-Demo-iOS/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Blurberry-Demo-iOS
+//
+//  Created by Pavel Puzyrev on 09.09.2020.
+//  Copyright © 2020 Pavel Puzyrev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ Demo/Blurberry-Demo-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Blurberry-Demo-iOS
+//
+//  Created by Pavel Puzyrev on 09.09.2020.
+//  Copyright © 2020 Pavel Puzyrev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
